02b41ac78705ab0a7ee3081241452add5f471f34
stefanie-weiss-neuland <stefanie.weiss@neuland-bfi.de>
2025-01-10 11:01:03 +0100

    Sprint Model init

--- Projektmanagement/models.py
+++ Projektmanagement/models.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from django.db import models
 # ToDo: correct following imports
 from django.db.models import Manager
@@ -24,3 +26,37 @@ class Task(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+
+class Sprint(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(null=False, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    hours = models.IntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # 'done' is true when end_date in the past
+        if self.end_date and self.end_date < date.today():
+         self.done = True
+        else:
+            self.done = False
+
+        self.hours = self.workhours()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+     return self.name
+
+    def workhours(self):
+        # No workdays between start_date und end_date.
+        if not self.start_date or not self.end_date:
+            return 0
+
+        workdays = 0
+        current_date = self.start_date
+        while current_date <= self.end_date:
+            if current_date.weekday() < 5:  # Monday=0, Sunday=6
+                workdays += 1
+            current_date += timedelta(days=1)
+
+        return workdays * 8 # 8-Hour Days
