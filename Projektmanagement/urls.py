22cb4cb264e20a4fb3bcc7c2a0df2850c0ee4e01
stefanie-weiss-neuland <stefanie.weiss@neuland-bfi.de>
2025-01-24 11:27:29 +0100

    started views

--- Projektmanagement/urls.py
+++ Projektmanagement/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Projektmanagement import views
+
 urlpatterns = [
+    path('/', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('project/', views.project, name='project'),
+    path('task/', views.task, name='task'),
 ]
