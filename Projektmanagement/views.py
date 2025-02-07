c392af288950d5ec7184a44b17bde8b7b824845d
stefanie-weiss-neuland <stefanie.weiss@neuland-bfi.de>
2025-02-07 10:35:48 +0100

    aufgeräumt

--- Projektmanagement/views.py
+++ Projektmanagement/views.py
@@ -1,18 +1,22 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 from Projektmanagement.models import Task
+
 
 def main_view(request):
     return render(request, 'baseTemplate.html')
 
+
 def project_overview(request):
     return render(request, 'Project/ProjectOverview.html')
+
 
 def project_create(request):
     return render(request, 'Project/ProjectCreate.html')
 
-def get_task(request, task_id):
+
+def task_detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
@@ -20,16 +24,9 @@ def get_task(request, task_id):
     return render(request, 'task.html', {'task': task})
 
 
-def task_list(request):
+def task_overview(request):
     try:
         tasks = Task.objects.all()
     except Task.DoesNotExist:
         raise Http404("Tasklist empty")
     return render(request, 'task_list.html', {'tasks': tasks})
-
-def home(request):
-    return None
-
-def project(request):
-    return None
-
