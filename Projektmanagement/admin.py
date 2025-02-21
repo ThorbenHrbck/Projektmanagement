c1588e54dc8d7014493c4862b94c6da78e8a3b55
stefanie-weiss-neuland <stefanie.weiss@neuland-bfi.de>
2025-02-21 20:34:43 +0100

    Registered models in the admin console

---
+++ Projektmanagement/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Projektmanagement.models import User,Project,Task,Sprint
+
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Sprint)
